dfa9af9cb11b70ad7730df00cc9e13ff438b0a4d
tarena <tarena@xiaoxiaosa.local>
2015-10-08 11:55:26 +0800

    hahah

---
+++ daf/daf/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  daf
+//
+//  Created by tarena on 10/8/15.
+//  Copyright (c) 2015 tarena. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ daf/dafTests/dafTests.m
@@ -0,0 +1,40 @@
+//
+//  dafTests.m
+//  dafTests
+//
+//  Created by tarena on 10/8/15.
+//  Copyright (c) 2015 tarena. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface dafTests : XCTestCase
+
+@end
+
+@implementation dafTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
